31afa32f859339b7e92b09cb743fd2bf73733a62
marcocraft1000-hue <marcocraft1000@gmail.com>
2026-05-22 01:13:39 -0600

    Update app.py

--- APP/app.py
+++ APP/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "WearSouth funcionando 🚀"
+    return "WearSouth online 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
